b4de2b8af1db27cef524085512dceec1df8e2c68
Eujine <sponsor85@mail.ru>
2019-03-12 02:46:08 +0200

    [*] improve solution for p04

--- HomeWork_2/p04/P04.erl
+++ HomeWork_2/p04/P04.erl
@@ -1,22 +1,15 @@
+%% P04 (*) Посчитать количество элементов списка:
+%% Пример:
 %% 1> p04:len([]).
 %% 0
 %% 2> p04:len([a,b,c,d]).
 %% 4
 
-
 -module(p04).
 -export([len/1]).
 
-len([H|T]) ->
-    %%erlang:display("Start"),
-    %%erlang:display(H),
-    %%erlang:display(T),
-    lenEx(T,1);
-len([]) -> 0.
+len(L) -> len(L,0).
 
-lenEx([H|T],I) ->
-    %%erlang:display(H),
-    %%erlang:display(T),
-    %%erlang:display(I),
-    lenEx(T, I + 1);
-lenEx([], I) -> I.
+len([_|T], C) -> 
+    len(T, C+1);
+len([],C) -> C.
