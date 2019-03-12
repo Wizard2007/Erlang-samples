26a347078ace24f0905371e86e668d153cc5f009
Eujine <sponsor85@mail.ru>
2019-03-12 02:10:25 +0200

    [+] solution for p01

---
+++ HomeWork_2/P01/P01.erl
@@ -0,0 +1,22 @@
+%% P01 (*) Найти последний элемент списка:
+%% Пример:
+%% 1> p01:last([a,b,c,d,e,f]).
+%% f
+
+-module(p01).
+-export([last/1]).
+
+last([H|T]) ->
+    %%erlang:display("Start"),
+    %%erlang:display(H),
+    %%erlang:display(T),
+    lastEx(H,T);
+last([]) -> false.
+
+lastEx(_,[H|T]) ->
+    %%erlang:display(H),
+    %%erlang:display(T),
+    lastEx(H, T);
+lastEx(H,[]) ->
+    %%erlang:display("Finish"),
+    H.
