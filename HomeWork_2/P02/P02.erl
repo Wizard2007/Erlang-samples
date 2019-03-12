77045c4de5cf9511f54f07115b31be7c4bbf257e
Eujine <sponsor85@mail.ru>
2019-03-12 02:39:55 +0200

    [*] improve solution for p02

--- HomeWork_2/P02/P02.erl
+++ HomeWork_2/P02/P02.erl
@@ -7,17 +7,8 @@
 -module(p02).
 -export([but_last/1]).
 
-but_last([H|[H1|[H2|T]]]) ->
-    %%erlang:display("Start"),
-    but_last_ex(H1,H2,T);
-but_last([H1|[]]) -> undefined;
-but_last([H|[H1|[]]]) ->
-    %%erlang:display("match #1 "),
-    [H,H1];
-but_last([]) -> undefined.
+but_last([_,_] = L) -> 
+    L;
+but_last([_|T]) ->    
+    but_last(T).
 
-but_last_ex(H,H1,[H2|T]) ->
-    but_last_ex(H1,H2,T);
-but_last_ex(H,H1,[]) ->
-    %%erlang:display("Finish"),
-    [H,H1].
