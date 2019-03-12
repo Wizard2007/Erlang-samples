bc76bdb1f15ec27f8e7984286b804abcb126d78a
Eujine <sponsor85@mail.ru>
2019-03-12 02:51:18 +0200

    [*] improve solution for p06

--- HomeWork_2/p06/P06.erl
+++ HomeWork_2/p06/P06.erl
@@ -3,30 +3,14 @@
 %% 1> p06:is_palindrome([1,2,3,2,1]).
 %% true
 
-
 -module(p06).
 -export([is_palindrome/1]).
 
-is_palindrome([H|T]) ->
-    %%erlang:display("Start"),
-    %%erlang:display(H),
-    %%erlang:display(T),
-    is_palindromeEx(T, [H]);
-is_palindrome([]) -> true.
+is_palindrome(L) -> reverse(L) == L.
 
-is_palindromeEx([H|T],R) ->
-    erlang:display(H),
-    erlang:display(T),
-    erlang:display(R),
-    erlang:display("------------"),
-    if 
-        [H|T] == R -> true;
-        true ->
-        if
-            R == T -> true;
-            true -> is_palindromeEx(T, [H|R])
-        end
-    end;
-is_palindromeEx([],_) ->
-    %%erlang:display("Finish"),
-    false.
+reverse(L) -> reverse(L,[]).
+
+reverse([H|T],Acc) -> 
+   reverse(T,[H|Acc]);
+reverse([],T) -> T.
+
