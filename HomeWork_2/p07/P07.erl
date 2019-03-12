c6964b839536b02ae670a4ffef63d9f1d43e800e
Eujine <sponsor85@mail.ru>
2019-03-12 02:58:31 +0200

    [*] improve solution for p07

--- HomeWork_2/p07/P07.erl
+++ HomeWork_2/p07/P07.erl
@@ -7,44 +7,9 @@
 -module(p07).
 -export([flatten/1]).
 
-flatten([H|T]) ->
-    erlang:display("# 0"),
-    erlang:display(H),
-    erlang:display(T),
-    erlang:display("-----------"),
-    flattenEx(H,T);
-flatten([]) ->
-    erlang:display("# 2"),
-    erlang:display("-----------"),
-    [].
+flatten(L) -> flatten(L,[]).
 
-flattenEx([H|T], TL) ->
-    erlang:display(H),
-    erlang:display(T),
-    erlang:display(TL),
-    erlang:display("# 1.1 list "),
-    erlang:display("-----------"),
-    flattenEx(H,[T|TL]);
-flattenEx(H,[H1|T]) ->
-    erlang:display(H),
-    erlang:display(H1),
-    erlang:display(T),
-    erlang:display("# 1.2 single element "),
-    erlang:display("-----------"),
-    flattenEx(H1,T);
-flattenEx([],T) ->
-    erlang:display(T),
-    erlang:display("# 1.3 empty element "),
-    erlang:display("-----------"),
-    flattenEx(T,[]);
-flattenEx([H|T],[]) ->
-    erlang:display(H),
-    erlang:display(T),
-    erlang:display("# 1.4 empty element "),
-    flattenEx(H,T);
-flattenEx(H,[]) ->
-    erlang:display(H),
-    erlang:display("# 1.5 empty element "),
-    erlang:display("# end");
-flattenEx([],[]) ->
-    erlang:display("# end").
+flatten([H|T], Acc) -> 
+   flatten(H, flatten(T,Acc));
+flatten([], T) -> T;
+flatten(H,Acc) -> [H|Acc].
