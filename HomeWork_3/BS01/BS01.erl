dde3886f16768046203675e00f40c09ffeabaa1f
Eujine <sponsor85@mail.ru>
2019-03-14 00:33:25 +0200

    [+] solution dor bs01

---
+++ HomeWork_3/BS01/BS01.erl
@@ -0,0 +1,15 @@
+%% https://www.tutorialspoint.com/erlang/erlang_binary_part.htm
+%% BS01: Извлечь из строки первое слово:
+%% Пример:
+%% 1> BinText = <<”Some text”>>.
+%% <<”Some Text”>>
+%% 2> bs01:first_word(BinText).
+%% <<”Some”>>
+
+-module(bs01).
+-export([first_word/1]).
+
+first_word(Bin) ->  binary_part(Bin,{0,search(Bin,0)}).
+
+search(<<" ", _/binary>>, C) ->  C;
+search(<<_, Rest/binary>>, C) ->  search(Rest, C + 1).
