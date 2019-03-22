8f62b5f130dd62ed9c4f14cec344bbfaebc67e42
Eujine <sponsor85@mail.ru>
2019-03-23 00:00:26 +0200

    [*] code clean up

--- HomeWork_5/my_cache/src/my_cache.erl
+++ HomeWork_5/my_cache/src/my_cache.erl
@@ -5,17 +5,10 @@
 %% ok = my_cache:delete_obsolete().       %% Очистка утстаревших данных
 -include_lib("stdlib/include/ms_transform.hrl").
 -module(my_cache).
-
--import(calendar,[datetime_to_gregorian_seconds/1,
-                  gregorian_seconds_to_datetime/1,
-                  universal_time/0]). 
+-import(calendar,[datetime_to_gregorian_seconds/1, gregorian_seconds_to_datetime/1, universal_time/0]). 
 
 %% API exports
--export([create/0,
-         insert/3, 
-         lookup/1, 
-         delete_obsolete/0
-        ]).
+-export([create/0, delete_obsolete/0, insert/3, lookup/1 ]).
 
 -record(my_cache_item,{value, expired_at}).
 
@@ -36,28 +29,18 @@ insert(Key, Val, TimeOut) ->
     ok.
 
 lookup(Key) ->
-    CurrentItem = ets:lookup(my_cache, Key),
-    if 
-        CurrentItem =:= [] ->
-            {ok, []};
-        true ->     
-            [{_, Item}] = CurrentItem,
-            CurrentDate = universal_time(),
-            if 
-                Item#my_cache_item.expired_at >= CurrentDate  -> 
-                    {ok, Item};
-                true ->
-                    {ok, []}
-            end
+    CurrentDate = universal_time(),
+    case ets:lookup(my_cache, Key) of
+        [{_,Val}] when Val#my_cache_item.expired_at >= CurrentDate -> {ok, Val};
+        _ -> undefined
     end.
-
     
 delete_obsolete() -> 
     CurrentDate = universal_time(),
-    Expr = ets:fun2ms(
-                      fun({Key, Item}) when Item#my_cache_item.expired_at < CurrentDate -> {Key, Item} end
-                     ),
-    ets:match_delete(my_cache, Expr),
+    ets:match_delete(my_cache, 
+        ets:fun2ms(
+            fun({Key, Item}) when Item#my_cache_item.expired_at < CurrentDate -> {Key, Item} end)
+        ),
     ok.
 
 %%====================================================================
@@ -71,13 +54,12 @@ addSecondsToDate(Date, TimeOut) -> gregorian_seconds_to_datetime(datetime_to_gre
 %%====================================================================
 
 -ifdef(TEST).
--include_lib("eunit/include/eunit.hrl").   
-
+-include_lib("eunit/include/eunit.hrl").
     my_cache_test_()->[?_assert(create() =:= ok),
                     ?_assert(ets:info(my_cache) /= undefined),
                     ?_assert(insert(key1,1,600) =:= ok),                    
                     ?_assert(lookup(key1) =:= {ok, #my_cache_item{value = 1, expired_at = addSecondsToDate(universal_time(),600)}}),
-                    ?_assert(lookup(key2) =:= {ok, []}),
+                    ?_assert(lookup(key2) =:= undefined),
                     ?_assert(delete_obsolete() =:= ok)
                    ].
 
@@ -93,5 +75,3 @@ addSecondsToDate(Date, TimeOut) -> gregorian_seconds_to_datetime(datetime_to_gre
         ?_assert(lookup(key3) =:= {ok, #my_cache_item{value = 3, expired_at = addSecondsToDate(universal_time(),60)}}),
         ?_assert(lookup(key4) =:= {ok, #my_cache_item{value = 4, expired_at = addSecondsToDate(universal_time(),60)}}).        
 -endif.
-
-%% http://erlang.org/pipermail/erlang-questions/2015-August/085570.html
