e761b6a6f3fdf15c0f596be8059400bd4f2bb1d5
Eujine <sponsor85@mail.ru>
2019-03-24 00:09:33 +0200

    [+] simple server with state

--- HomeWork_6/simple_server_with_state/README.md
+++ HomeWork_6/simple_server_with_state/README.md
@@ -3,6 +3,9 @@ simple_server_with_state
 
 An OTP library
 
+https://ru.hexlet.io/courses/erlang_101/lessons/practical_erlang_gen_server/theory_unit
+
+
 Build
 -----
 
--- HomeWork_6/simple_server_with_state/src/simple_server_with_state.erl
+++ HomeWork_6/simple_server_with_state/src/simple_server_with_state.erl
@@ -1,12 +1,32 @@
 -module(simple_server_with_state).
 
 %% API exports
--export([]).
+-export([start/0, loop/1]).
 
 %%====================================================================
 %% API functions
 %%====================================================================
 
+start() ->
+    InitialState = [],
+    spawn(?MODULE, loop, [InitialState]).
+
+loop(State) ->
+    receive 
+        {add, Item} -> io:format("~p adds to its state ~p~n", [self(), Item]),
+                       NewState = [Item|State],
+                       loop(NewState);
+        {remove, Item} -> NewState = case lists:member(Item, State) of
+                                         true -> lists:delete(Item, State);
+                                         false -> io:format("I have no ~p~n", [Item]),
+                                                  State
+                                     end,
+                          loop(NewState);
+        show_items -> io:format("my  items is ~p~n",[State]),
+                      loop(State);
+        stop -> io:format("~p stops now ~n", [self()]);
+        _Any -> loop(State)
+    end.
 
 %%====================================================================
 %% Internal functions
