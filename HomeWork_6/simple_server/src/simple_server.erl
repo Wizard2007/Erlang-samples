384fe59b8a82d3eee16474a1207c45778962ccbe
Eujine <sponsor85@mail.ru>
2019-03-23 23:48:36 +0200

    [+] simple server with proess mesage functionality

--- HomeWork_6/simple_server/src/simple_server.erl
+++ HomeWork_6/simple_server/src/simple_server.erl
@@ -1,7 +1,19 @@
 -module(simple_server).
 
 %% API exports
--export([]).
+-export([start/0]).
+
+start() ->
+    io:format("start ~p~n", [self()]),
+    spawn(fun loop/0).
+
+loop() ->
+    io:format("~p enter loop ~n", [self()]),
+    receive
+        stop -> io:format("~p stops now ~n", [self()]);
+        Msg -> io:format("~p receive ~p~n",[self(), Msg]),
+               loop()
+    end.
 
 %%====================================================================
 %% API functions
