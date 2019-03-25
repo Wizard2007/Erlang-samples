2c8842826c3a6f55a087228fce42e863002a2690
Eujine <sponsor85@mail.ru>
2019-03-25 03:18:32 +0200

    [*] add dummy api method
    [+] pereodicaly called function

--- HomeWork_6/cache_server/src/cache_server.erl
+++ HomeWork_6/cache_server/src/cache_server.erl
@@ -8,13 +8,16 @@
 %% Сервер должен хранить данные то количество времени которе было указано при записи.
 %% Интервал очистки устаревших данных указывается при старте (drop_interval). Время
 %% задается в секундах.
+-define(INTERVAL, 6000).
+
 -module(cache_server).
+-behaviour(gen_server).
 
 -record(state, {}).
 
 
 %% API exports
--export([start_link/0,init/1]).
+-export([start_link/0,init/1,my_api_1/1,handle_call/3,handle_info/2]).
 
 %%====================================================================
 %% API functions
@@ -23,7 +26,20 @@
 start_link() -> gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 init([]) ->
+    erlang:send_after(?INTERVAL, self(), trigger),
     {ok, #state{}}.
+
+my_api_1(A) ->
+    erlang:display("my_api_1~n"),
+    gen_server:call(?MODULE, {msg1, A}).
+
+handle_info(trigger, State) ->
+   erlang:display("trigger"),
+   erlang:send_after(?INTERVAL, self(), trigger),
+   {noreply, State}.
+
+handle_call({msg1, A}, _From, State) -> erlang:display("test"),
+    {reply, State, [A, _From]}.
 
 %%====================================================================
 %% Internal functions
